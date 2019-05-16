58fe48a9f21793bb0176656ff89e2855d41684d5
Nikolai Romashchenko <kolya.romashenko@yandex.ru>
2019-05-16 19:50:05 +0300

    upd

--- src/ru/bstu/it31/romashenko/lab4/Main.java
+++ src/ru/bstu/it31/romashenko/lab4/Main.java
@@ -1,8 +1,46 @@
 package ru.bstu.it31.romashenko.lab4;
 
-public class Main {
+import java.util.Scanner;
+import java.io.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        // Переменная для хренения анализируемого текста
+        String content = "";
+        String resualt = "";
+        // Считываем данные из файла
+        try (Scanner scannerFile = new Scanner( new File("index.html"), "UTF-8" )) {
+            content = scannerFile.useDelimiter("\\A").next();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+        // Инизиализует сканнер для консоли
+        Scanner scanner = new Scanner(System.in);
+        // Анализ ссылок в теге <a>
+        Pattern patternUrlTagA = Pattern.compile("<a(.*?)href=([\'\"])(.*?)([\'\"])(.*?)>(.*?)</a>");
+        Matcher matcher = patternUrlTagA.matcher(content);
+        while (matcher.find()) {
+            resualt += matcher.group(3) + " " + matcher.group(6) + "\n";
+        }
+        // Анализ ссылок отличных от тега <a>
+        patternUrlTagA = Pattern.compile("<[^a]*(.*?)>(.*?)[\\s]?(http://[a-z0-9/?=-_&.]+)[\\s]?(.*?)</[^a]*>");
+        matcher = patternUrlTagA.matcher(content);
+        while (matcher.find()) {
+            resualt += matcher.group(3) + "\n";
+        }
+        // Вывод на экран результат
+        System.out.println(resualt);
+        // Запись в файл результат
+        try (PrintWriter printer = new PrintWriter("output.txt");) {
+            printer.write(resualt);
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+        // Конец
+        scanner.close();
     }
 }
